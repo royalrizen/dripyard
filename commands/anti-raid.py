0b8e0da30297430005f7117aed9c65497f16235c
Rizen <123223112+royalrizen@users.noreply.github.com>
2024-12-28 12:50:32 +0530

    Update anti-raid.py

--- commands/anti-raid.py
+++ commands/anti-raid.py
@@ -8,7 +8,6 @@ class AntiRaid(commands.Cog):
         self.bot = bot
 
     @commands.command(name="massban", aliases=['mb'], usage="<message>", description="Mass bans all the users who sent a specific message")
-    @commands.check(is_staff)
     async def mass_ban(self, ctx, *, target_message: str):
         """Bans users who sent a certain message."""
         if not ctx.guild:
